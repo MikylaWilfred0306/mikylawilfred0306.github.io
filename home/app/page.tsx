bca25dc415a7e9181435df5c4808dcb91dcb748c
Mikyla R Wilfred <mikyla.wilfred@gmail.com>
2025-02-15 16:11:46 -0500

    Cleaned up

--- home/app/page.tsx
+++ home/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div> <h1>Yay!</h1>
-    <p>I'm hosted with GitHub Pages.</p></div>
+    <div> 
+      <h1>Yay!</h1>
+      <p>I am hosted with GitHub Pages.</p>
+    </div>
   );
 }
